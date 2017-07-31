b1605711a57c30b5b7b6aad87f4f0464541de53c
kyle roux <jstacoder@gmail.com>
2017-07-31 13:43:30 -0700

    styles and trying to add FOCUSED

--- PROJECT/src/components/todos/todo-form.jsx
+++ PROJECT/src/components/todos/todo-form.jsx
@@ -1,22 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Form, Col, FormGroup, FormControl, ControlLabel } from 'react-bootstrap';
 
-const TodoForm = (props) =>{
-    const onSubmit = (e) =>{
+class TodoForm extends Component{
+	constructor(props){
+		super(props);
+		this.state = {
+			focused: false
+		};
+	}
+	setFocus = () =>{ 		
+		this.setState(
+			{focused: true}
+		);
+	}
+	setBlur = () =>{
+		this.setState(
+			{focused: false}
+		);
+	}
+	onSubmit = (e) =>{
 		e.preventDefault();  
-		props.actions.addTodo({todo: props.todos.currentTodo, index: props.todos.todos.length});	
-		props.actions.setCurrentTodo({text: '', todoState: ''});
-    };
-	return (
-	    <Form onSubmit={onSubmit} horizontal>
-			<FormGroup> 
-				<ControlLabel>todo</ControlLabel>
-				<FormControl
-					value={props.todos.currentTodo.text} 
-					onChange={props.changeTodo} 
-					type="text" />
-			</FormGroup>
-        </Form>
-	);
-};
- export default TodoForm
+		this.props.actions.addTodo({todo: this.props.todos.currentTodo, index: this.props.todos.todos.length});	
+		this.props.actions.setCurrentTodo({text: '', todoState: ''});
+	}
+	render(){    
+		const inputStyle = {
+			borderLeft: '0px',
+			borderRight: '0px',
+			borderRadius: '0px',
+			borderTop: '0px',
+			borderBottom: '.3px solid #eee',
+			boxShadow:'none',
+			transition: 'initial',
+		};
+		const focus = this.state.focused ? 'IN FOCUS' : 'NOT FOCUSED';
+		return (
+				<Form onSubmit={this.onSubmit} horizontal>
+				<FormGroup> 
+				  <p>{focus}</p>
+					<ControlLabel>todo</ControlLabel>
+					<FormControl
+						onFocus={this.setFocus}
+						onBlur={this.setBlur}
+						style={inputStyle}
+						value={this.props.todos.currentTodo.text} 
+						onChange={this.props.changeTodo} 
+						type="text" />
+				</FormGroup>
+					</Form>
+		);
+	}
+}
+export default TodoForm
